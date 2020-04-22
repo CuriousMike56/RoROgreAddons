fb370962c9938ba17c083b07f369591a70e8d96a
CuriousMike56 <46073351+CuriousMike56@users.noreply.github.com>
2020-04-21 22:24:45 -0400

    Add myself to credits

--- io_export_ogreDotScene.py
+++ io_export_ogreDotScene.py
@@ -1,5 +1,6 @@
 
 # Copyright (C) 2010 Brett Hartshorn
+# Rigs of Rods updates: Michael (CuriousMike)
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
